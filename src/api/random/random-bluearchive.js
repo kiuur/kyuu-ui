f10b676b92d5a48d98028c707c5fcc4b77589cb4
K <kyuurzy@gmail.com>
2025-10-05 01:55:31 +0800

    Update random-bluearchive.js

--- src/api/random/random-bluearchive.js
+++ src/api/random/random-bluearchive.js
@@ -3,7 +3,9 @@ module.exports = function(app) {
     async function bluearchive() {
         try {
             const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`)
-            const response = await axios.get(data[Math.floor(data.length * Math.random())], { responseType: 'arraybuffer' });
+            const response = await axios.get(data[Math.floor(data.length * Math.random())], {
+                responseType: 'arraybuffer' 
+            });
             return Buffer.from(response.data);
         } catch (error) {
             throw error;
