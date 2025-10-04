36c29598fa872bd32e3c488a40f63ba474d1d034
K <kyuurzy@gmail.com>
2025-10-05 01:47:00 +0800

    Update search-youtube.js

--- src/api/search/search-youtube.js
+++ src/api/search/search-youtube.js
@@ -3,7 +3,10 @@ module.exports = function(app) {
     app.get('/search/youtube', async (req, res) => {
         const { q } = req.query;
         if (!q) {
-            return res.status(400).json({ status: false, error: 'Query is required' });
+            return res.status(400).json({
+                status: false, 
+                error: 'Query is required'
+            });
         }
         try {
             const ytResults = await yts.search(q);
@@ -19,7 +22,10 @@ module.exports = function(app) {
                 result: ytTracks
             });
         } catch (error) {
-            res.status(500).json({ status: false, error: error.message });
+            res.status(500).json({ 
+                status: false, 
+                error: error.message
+            });
         }
     });
 }
